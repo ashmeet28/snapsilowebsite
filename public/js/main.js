7340994c69087d952882549894479ec5749a51ee
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-03-09 02:31:47 -0500

    Progess

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-console.log("hi")
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./react-comp/App.jsx";
+
+const root = createRoot(document.getElementById("mainreactroot"));
+root.render(
+    <App />
+);
