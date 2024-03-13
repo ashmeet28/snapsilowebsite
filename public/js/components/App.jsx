0a3901942113fbb3a41451be3dcb003df0121f01
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-03-12 22:57:26 -0400

    Added sign in code

--- public/js/components/App.jsx
+++ public/js/components/App.jsx
@@ -10,16 +10,34 @@ export default function MyApp() {
         CHECK_IF_USER_SIGNED_IN: 2,
         SIGN_IN_ENTER_EMAIL: 3,
         SIGN_IN_ENTER_CODE: 4,
+        USER_IMAGES: 5,
+        USER_UPLOAD_IMAGE: 6,
     };
 
     const [currAppSt, setCurrAppSt] = useState(APP_ST.SEARCH_IMAGES);
-
+    const [userAuthToken, setUserAuthToken] = useState('');
+    const [isUserAuthTokenInit, setIsUserAuthTokenInit] = useState(true);
+    const [userEmail, setUserEmail] = useState('');
 
     useEffect(() => {
-        if (currAppSt === APP_ST.CHECK_IF_USER_SIGNED_IN) {
-            setCurrAppSt(APP_ST.SIGN_IN_ENTER_EMAIL);
+        if (isUserAuthTokenInit) {
+            localStorage.setItem('user_auth_token', userAuthToken);
+        } else {
+            if (localStorage.getItem('user_auth_token') !== null) {
+                setUserAuthToken(localStorage.getItem('user_auth_token'));
+            }
+            setIsUserAuthTokenInit(true);
         }
 
+        return () => { };
+    }, [userAuthToken]);
+
+    useEffect(() => {
+        switch (currAppSt) {
+            case APP_ST.CHECK_IF_USER_SIGNED_IN: {
+                setCurrAppSt(APP_ST.SIGN_IN_ENTER_EMAIL);
+            }
+        }
         return () => { };
     }, [currAppSt]);
 
@@ -32,10 +50,10 @@ export default function MyApp() {
                 </div>
 
                 <div className="flex flex-row">
-                    {currAppSt === APP_ST.SEARCH_IMAGES &&
+                    {currAppSt === APP_ST.SEARCH_IMAGES ?
                         <img onClick={() => setCurrAppSt(APP_ST.CHECK_IF_USER_SIGNED_IN)}
-                            className="h-12 opacity-75 cursor-pointer" src="/svg/person-circle-outline.svg" alt="person icon" />}
-                    {(currAppSt !== APP_ST.SEARCH_IMAGES) &&
+                            className="h-12 opacity-75 cursor-pointer" src="/svg/person-circle-outline.svg" alt="person icon" />
+                        :
                         <img onClick={() => setCurrAppSt(APP_ST.SEARCH_IMAGES)}
                             className="h-12 opacity-75 cursor-pointer" src="/svg/globe-outline.svg" alt="globe icon" />}
                 </div>
@@ -48,12 +66,26 @@ export default function MyApp() {
                     <h1 className="text-center p-2 text-4xl my-2">Sign In</h1>
 
                     {currAppSt === APP_ST.SIGN_IN_ENTER_EMAIL &&
-                        <SignInEnterEmail onEmailEntered={(v) => { console.log(v); setCurrAppSt(APP_ST.SIGN_IN_ENTER_CODE); }}></SignInEnterEmail>}
+                        <SignInEnterEmail onEmailEntered={(v) => {
+                            fetch("/api/send-code", { method: "POST", body: JSON.stringify({ email_address: v }) });
+                            setUserEmail(v);
+                            setCurrAppSt(APP_ST.SIGN_IN_ENTER_CODE);
+                        }}></SignInEnterEmail>}
 
                     {currAppSt === APP_ST.SIGN_IN_ENTER_CODE &&
-                        <SignInEnterCode onCodeEntered={(v) => console.log(v)}></SignInEnterCode>}
+                        <SignInEnterCode onCodeEntered={(v) => {
+                            fetch("/api/verify-code", {
+                                method: "POST", body: JSON.stringify({ email_address: userEmail, verification_code: v })
+                            }).then((res) => {
+                                if (res.status === 200) {
+                                    res.json().then((data) => {
+                                        setUserAuthToken(data.auth_token);
+                                    })
+                                }
+                            })
+                        }}></SignInEnterCode>}
                 </div>
             }
-        </div>
+        </div >
     );
 }
