a39d85a5d4958a13e5d84040930a9cf3ecdb543c
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-04-18 11:14:58 -0400

    Progress

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ A stock photos website
 
 # Setup dev environment
 ```
-npx tailwindcss -i ./public/css/tw-input.css -o ./public/css/tw-output.css --watch
+npx tailwindcss -i ./src/css/tw-input.css -o ./src/css/tw-output.css --watch
 npx webpack --watch
 ```
--- src/components/UserDash.jsx
+++ src/components/UserDash.jsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from "react";
 
-import UserMenuBox from "./UserMenuBox.jsx";
+export default function UserDash({ userAuthToken, onSignOut }) {
+    const S_PROFILE = 1;
+    const S_IMAGES = 2;
+    const S_UPLOAD = 3;
 
-export default function UserDash({ onSignOut }) {
+    const [currState, setCurrState] = useState(S_IMAGES);
+
     return (
         <>
-            <UserMenuBox></UserMenuBox>
+            <div className="flex flex-row justify-center gap-4 py-2 px-4 shadow rounded">
+                <div className="flex items-center justify-center gap-1 cursor-pointer" onClick={setCurrState(S_PROFILE)}>
+                    <img className="w-8 cursor-pointer opacity-75" src="/svg/person-circle-outline.svg" alt="" />
+                    <h3 className="text-lg">Profile</h3>
+                </div>
+                <div className="flex items-center justify-center gap-1 cursor-pointer" onClick={setCurrState(S_IMAGES)}>
+                    <img className="w-8 cursor-pointer opacity-75" src="/svg/image-outline.svg" alt="" />
+                    <h3 className="text-lg">Images</h3>
+                </div>
+                <div className="flex items-center justify-center gap-1 cursor-pointer" onClick={setCurrState(S_UPLOAD)}>
+                    <img className="w-8 cursor-pointer opacity-75" src="/svg/cloud-upload-outline.svg" alt="" />
+                    <h3 className="text-lg">Upload</h3>
+                </div>
+            </div>
         </>
     )
 }
